365633526acd05ef8939930cc2974ab69b28ab81
Brian Eugene Smith <brianesmith11@gmail.com>
2015-10-24 13:12:31 -0500

    updates

--- app/day.py
+++ app/day.py
@@ -4,12 +4,14 @@ from app.database import *
 from app.scale import *
 
 def current_date():
-    result = engine.execute('select current_date').first()
+    result = engine.execute('SELECT current_date').first()
     newScale = choose_scale()
 
     if result == None or result == []:
         name = newScale.scale_name
-        engine.execute('insert into days values(1,current_date,:1)',name)
+        engine.execute('INSERT INTO days VALUES(((SELECT COUNT(*) FROM days)+1),current_date,%s)',name)
+
+
 
         return select_piece(name)
     else:
@@ -22,11 +24,13 @@ def choose_scale():
     return scale(s,m)
 
 def select_piece(k):
-    key = engine.execute('select scale from days where day = current_date').first()
-    result = engine.execute('select * from pieces where key like %s order by random() limit 1',key[0]).first()
+    key = engine.execute('SELECT  scale FROM days WHERE day = current_date').first()
+    result = engine.execute('SELECT * FROM pieces WHERE key LIKE %s ORDER BY random() LIMIT 1',key[0]).first()
+    s = ''
 
     if result == None or result == []:
-        return [(newScale + ' is unfortunately not in the database. Check out some recommendations below, though.'),k]
+        s = ('%s is unfortunately not in the database. Check out some recommendations below, though.' % (k))
     else:
         s = ("%s (%i) by %s, Op.%i") % (result[1],result[5],result[3],result[6])
-        return [s,k]
+
+    return [s,k]
