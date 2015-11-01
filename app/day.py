16356f692279483fec3d9ec1990c303496ef4249
Brian Eugene Smith <brianesmith11@gmail.com>
2015-11-01 15:24:12 -0600

    app

--- app/day.py
+++ app/day.py
@@ -1,6 +1,6 @@
 from random import choice
 
-from app.database import *
+from app.database_prod import *
 from app.scale import *
 
 def current_date():
@@ -50,6 +50,5 @@ def select_piece(k):
         video_key = (result[6])
     else:
         video_key = 'JNsKvZo6MDs'
-
 
     return [s,k,key[0],video_key]
